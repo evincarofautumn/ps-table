72cecaca2f04f07ef4f90aaa6e8d76db5558f191
Jon Purdy <evincarofautumn@gmail.com>
2014-10-31 13:30:24 -0700

    Add some command-line options.

--- ps-table.pl
+++ ps-table.pl
@@ -3,11 +3,25 @@
 use warnings;
 use strict;
 
+use Getopt::Long;
 use POSIX qw(strftime :sys_wait_h);
 use Pod::Usage;
 use Time::HiRes qw(gettimeofday);
 
-pod2usage(2) if @ARGV != 1;
+my $help = 0;
+my $working_directory = '';
+my $absolute_timestamps = 0;
+
+GetOptions
+  (
+   'absolute-timestamps' => \$absolute_timestamps,
+   'chdir|d=s' => \$working_directory,
+   'help|?' => \$help,
+  ) or pod2usage(1);
+
+pod2usage(0) if $help;
+
+chdir $working_directory if $working_directory;
 
 my $command = shift @ARGV;
 my $pid = 0;
@@ -31,7 +45,7 @@ until (eof $process or $kid < 0) {
 	printf STDERR "warning: ps exited with nonzero status %d\n", $? >> 8;
 	last;
   }
-  $timestamp -= $start_time;
+  $timestamp -= $start_time unless $absolute_timestamps;
   printf "%.3f\t%d\n", $timestamp, $rss;
   $kid = waitpid($pid, WNOHANG);
 }
@@ -44,15 +58,23 @@ ps-table.pl - A tool for getting a table of the resident set size (RSS) of a pro
 
 =head1 SYNOPSIS
 
-    ps-table.pl COMMAND
+    ps-table.pl [OPTIONS] COMMAND
 
 =head2 Options
 
 =over 4
 
-=item B<COMMAND>
+=item I<COMMAND>
 
 The shell command to run.
+
+=item B<--absolute-timestamps>
+
+Use absolute, not relative, timestamps.
+
+=item B<--chdir> I<DIR>, B<-d> I<DIR>
+
+Change the working directory to I<DIR> before running I<COMMAND>.
 
 =back
 
